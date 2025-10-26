a241bf2caaa71dce7b22b30f28680b77851105d4
Felipe Vega Rivera <fvegarivera@gmail.com>
2025-10-25 20:36:53 -0500

    funcion agregada

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola mis chicas lindas!!!!");
-    }
+        System.out.println("Hola mis chicas lindas!!!!");
+        System.out.println("Modificación 1 para nueva rama!!!!");
+        System.out.println("Se agregó una nueva función para la nueva rama");
 }
